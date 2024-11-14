a5b4a12399f55284f7fbb51e042521a1b712b3ca
Crosado1991 <135192649+Crosado1991@users.noreply.github.com>
2024-11-14 13:54:09 -0500

    Update projects.js

--- projects.js
+++ projects.js
@@ -3,6 +3,7 @@ const projects = {
         { title: "Python Snake Game", description: "A classic Snake Game built in Python.", link: "https://github.com/Crosado1991/Snake_Game.git", imgSrc: "images/snake-game.jpg" },
         { title: "Cheap Flight Finder", description: "A flight search tool to find affordable flights.", link: "https://github.com/Crosado1991/CheapFlightFinder.git", imgSrc: "images/cheap-flight-finder.jpg" },
         { title: "Password Manager", description: "Securely manage passwords.", link: "https://github.com/Crosado1991/Password-Manager.git", imgSrc: "images/password-manager.jpg" },
-        { title: "KnightQuest", description: "2-D Game using Pygame.", link: "https://github.com/Crosado1991/Knight-Quest.git", imgSrc: "images/knight-quest.jpg" }
+        { title: "KnightQuest", description: "2-D Game using Pygame.", link: "https://github.com/Crosado1991/Knight-Quest.git", imgSrc: "images/knight-quest.jpg" },
+        { title: "Playlist Creator", description: "Scrapes a playlist from Billboard's Top 100 chart for any specified date.", link: "https://github.com/Crosado1991/Time-Travel-Playlist-Creator.git", imgSrc: "images/playlist.jpg" }
     ]
 };
